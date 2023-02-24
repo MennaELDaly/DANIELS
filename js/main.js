b5407e14f2bc5d2f361d952164fb96eee0148a89
MennaELDaly <mennaeldaly101@gmail.com>
2023-02-25 01:33:20 +0200

    second commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+/// to change backgrund color in navbar when scroll
+
+$(window).scroll(function(){
+  $('nav').toggleClass('scrolled',$(this).scrollTop()>50);
+});
+
+// text in home section 
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 2px solid #fff}";
+    document.body.appendChild(css);
+};
+
+//magnific-popup
+
+var groups = {};
+$('.galleryItem').each(function() {
+  var id = parseInt($(this).attr('data-group'), 10);
+  
+  if(!groups[id]) {
+    groups[id] = [];
+  } 
+  
+  groups[id].push( this );
+});
+
+
+$.each(groups, function() {
+  
+  $(this).magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: false,
+      gallery: { enabled:true }
+  })
+  
+});
+
+
+
+///
+
+
+
+
+
+
+
+
+
